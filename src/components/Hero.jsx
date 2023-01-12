a05b5f73d3b25191278d8fe79c26b21f86e84a39
Jengas2015 <arejang@gmail.com>
2023-01-12 16:55:46 -0600

    1/12 work
    tentatively finished navbar. Began work on Hero page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hero () {
+    return (
+        <div className=''>
+        <h1>Hero Content</h1>
+        </div>
+    )
+}
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,9 +14,9 @@ function Navbar() {
             <div className="menu-items flex justify-start">
             <img src={logo} alt="logo" className='p-3' />
                 <ul className='menu-container flex ml-3 text-mediumGray'>
-                    <ul className='flex ml-20 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer hover:text-almostBlack'>
-                        Features <img className='downArrow self-center ml-2 transition' src={downArrow} />
-                        <ul className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 right-0 rounded-lg w-40 text-start text-mediumGray' >
+                    <ul className='flex ml-20 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer transition ease-in-out duration-300 hover:text-almostBlack'>
+                        Features <img className='relative bottom-2 self-center ml-2 transition' src={downArrow} />
+                        <ul className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 right-0 rounded-lg w-40 text-start text-mediumGray hidden' >
                             <li className='flex py-1'>
                                 <img className='mr-3 h-4 rotate-180' src={toDo} />Todo List
                             </li>
@@ -32,25 +32,25 @@ function Navbar() {
                         </ul>
 
                     </ul >
-                    <ul className='flex ml-8 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer hover:text-almostBlack'>
-                        Company <img className='self-center ml-2 transition' src={downArrow} />
-                        <div className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 left-0 rounded-xl text-mediumGray'>
+                    <ul className='flex ml-8 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer transition ease-in-out duration-300 hover:text-almostBlack'>
+                        Company <img className='relative bottom-2 self-center ml-2 transition duration-300' src={downArrow} />
+                        <ul className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 left-0 w-36 rounded-xl text-mediumGray hidden'>
                             <li className='py-1'>History</li>
                             <li className='py-1'>Our Team</li>
                             <li className='py-1'>Blog</li>
-                        </div>
+                        </ul>
                     </ul>
-                    <ul className='ml-8 p-3 cursor-pointer hover:text-almostBlack'>
+                    <ul className='ml-8 p-3 cursor-pointer transition ease-in-out duration-300 hover:text-almostBlack'>
                         Careers
                     </ul>
-                    <ul className='ml-8 p-3 cursor-pointer hover:text-almostBlack'>
+                    <ul className='ml-8 p-3 cursor-pointer transition ease-in-out duration-300 hover:text-almostBlack'>
                         About
                     </ul>
                 </ul>
             </div>
-            <div className="cta-buttons">
-                <button>Login</button>
-                <button>Register</button>
+            <div className="cta-buttons p-3 mr-5 text-mediumGray">
+                <button className='mr-10 transition ease-in-out duration-300 hover:text-almostBlack'>Login</button>
+                <button className='border-2 border-mediumGray py-1.5 px-4 rounded-lg transition ease-in-out duration-300 hover:text-almostBlack hover:border-almostBlack'>Register</button>
             </div>
         </div>
     )
