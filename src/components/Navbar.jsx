19a37c873a1947a381522d870002c321d5d0d0d2
Jengas2015 <arejang@gmail.com>
2023-01-11 18:43:31 -0600

    Update Navbar.jsx
    Added rotational animation for arrows along with additional stylings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,49 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
+import downArrow from '../assets/icon-arrow-down.svg'
+import toDo from '../assets/icon-todo.svg'
+import calendar from '../assets/icon-calendar.svg'
+import reminder from '../assets/icon-reminders.svg'
+import planning from '../assets/icon-planning.svg'
+
+
 function Navbar() {
     return (
-        <div className='flex justify-between p-5'>
+        <div className='flex justify-between'>
 
             <div className="menu-items flex justify-start">
-            <img src={logo} alt="logo" className='max-h-7' />
+            <img src={logo} alt="logo" className='p-3' />
                 <ul className='menu-container flex ml-3 text-mediumGray'>
-                    <ul className='ml-20 relative items-start'>
-                        Features
-                        <ul className='sub-menu mt-3 bg-white absolute shadow-lg p-5 right-0' >
-                            <li>
-                                Todo List
+                    <ul className='flex ml-20 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer hover:text-almostBlack'>
+                        Features <img className='downArrow self-center ml-2 transition' src={downArrow} />
+                        <ul className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 right-0 rounded-lg w-40 text-start text-mediumGray' >
+                            <li className='flex py-1'>
+                                <img className='mr-3 h-4 rotate-180' src={toDo} />Todo List
                             </li>
-                            <li>
-                                Calendar
+                            <li className='flex py-1'>
+                            <img className='mr-3 h-4 rotate-180' src={calendar} />Calendar
                             </li>
-                            <li>
-                                Reminders
+                            <li className='flex py-1'>
+                            <img className='mr-3 h-4 rotate-180' src={reminder} />Reminders
                             </li>
-                            <li>
-                                Planning
+                            <li className='flex py-1'>
+                            <img className='mr-3 h-4 rotate-180' src={planning} />Planning
                             </li>
                         </ul>
 
                     </ul >
-                    <ul className='ml-12 relative'>
-                        Company
-                        <div className='sub-menu mt-3 bg-white absolute shadow-lg'>
-                            <li>History</li>
-                            <li>Our Team</li>
-                            <li>Blog</li>
+                    <ul className='flex ml-8 relative [&_img]:hover:rotate-180 [&_ul]:hover:block p-3 cursor-pointer hover:text-almostBlack'>
+                        Company <img className='self-center ml-2 transition' src={downArrow} />
+                        <div className='sub-menu mt-9 bg-white absolute shadow-2xl p-5 left-0 rounded-xl text-mediumGray'>
+                            <li className='py-1'>History</li>
+                            <li className='py-1'>Our Team</li>
+                            <li className='py-1'>Blog</li>
                         </div>
                     </ul>
-                    <ul className='ml-8'>
+                    <ul className='ml-8 p-3 cursor-pointer hover:text-almostBlack'>
                         Careers
                     </ul>
-                    <ul className='ml-8'>
+                    <ul className='ml-8 p-3 cursor-pointer hover:text-almostBlack'>
                         About
                     </ul>
                 </ul>
